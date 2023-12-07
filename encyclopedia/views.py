2636be39e0df9af8c9f7b8bab7656c48c55642df
UncleLion <unclelion@ukr.net>
2023-12-07 23:06:29 +0300

    add new html, and functions

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from markdown import markdown
 
 from . import util
 
@@ -8,3 +9,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def title(request, title):
+    title_name = f"{title}"
+    text_html = util.get_entry(title)
+    if text_html is None:
+        text_html =f"Page {title} has not been found!"
+    else:
+        text_html = markdown(open(f"entries/{title}.md").read())
+    return render(request, "encyclopedia/title.html", {
+        "title_name": title_name,
+        "text_html": text_html,
+        "entries": util.list_entries(),
+        "form": NewSearchForm()
+    })
--- entries/JavaScript.md
+++ entries/JavaScript.md
@@ -11,6 +11,5 @@ JavaScript (JS) is a lightweight interpreted **or just-in-time compiled** progra
 * Classes
 
 ### links
-Django is a web framework written using [Python](/wiki/Python) that allows for the design of web applications that generate [HTML](/wiki/HTML) dynamically.
 
 JavaScript is a prototype-based,[CSS](/wiki/CSS) multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles.
